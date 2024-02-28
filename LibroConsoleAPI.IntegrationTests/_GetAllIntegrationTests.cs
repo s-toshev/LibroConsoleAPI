18faaf1fd845d3c0fa129b4a554c7147551865be
s-toshev <simeon.toshev@yahoo.bg>
2024-02-28 09:53:03 +0200

    28 Feb

--- LibroConsoleAPI.IntegrationTests/_GetAllIntegrationTests.cs
+++ LibroConsoleAPI.IntegrationTests/_GetAllIntegrationTests.cs
@@ -1,4 +1,6 @@
-﻿using LibroConsoleAPI.Business.Contracts;
+﻿using LibroConsoleAPI.Business;
+using LibroConsoleAPI.Business.Contracts;
+using LibroConsoleAPI.DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,29 +13,59 @@ namespace LibroConsoleAPI.IntegrationTests.XUnit
     {
         private readonly BookManagerFixture _fixture;
         private readonly TestLibroDbContext _dbContext;
-        private readonly IBookManager _bookManager;
+        private readonly BookManager _bookManager;
 
-        public _GetAllIntegrationTests() 
+        public _GetAllIntegrationTests()
         {
             _fixture = new BookManagerFixture();
-            _dbContext= _fixture.DbContext;
-            _bookManager= _fixture.BookManager;
-            
+            _dbContext = _fixture.DbContext;
+            _bookManager = _fixture.BookManager;
+
         }
+
+        //Arrange
+
+        //Act
+
+        //Assert
 
         [Fact]
         public async Task GetAllAsync_WhenBooksExist_ShouldReturnAllBooks()
         {
-            throw new NotImplementedException();
+
+            //Arrange
+            await DatabaseSeeder.SeedDatabaseAsync(_dbContext, _bookManager);
+
+            //Act
+            var allBooks = await _bookManager.GetAllAsync();
+
+            //Assert
+            Assert.True(_dbContext.Books.Count() == 10);
+            Assert.Equal(allBooks.Count(), 10);
+
+            foreach (var book in allBooks)
+            {
+                Assert.Contains(_dbContext.Books, b => b.Id == book.Id);
+                Assert.Contains(_dbContext.Books, b => b.ISBN == book.ISBN);
+                Assert.Contains(_dbContext.Books, b => b.Title == book.Title);
+                Assert.Contains(_dbContext.Books, b => b.Pages == book.Pages);
+                Assert.Contains(_dbContext.Books, b => b.Price == book.Price);
+                Assert.Contains(_dbContext.Books, b => b.Genre == book.Genre);
+                Assert.Contains(_dbContext.Books, b => b.YearPublished == book.YearPublished);
+
+            }
+
         }
 
 
         [Fact]
         public async Task GetAllAsync_WhenNoBooksExist_ShouldThrowKeyNotFoundException()
         {
-            throw new NotImplementedException();
+
+            //Act&Assert        
+            var exception = await Assert.ThrowsAsync<KeyNotFoundException>(() => _bookManager.GetAllAsync());
+            Assert.Equal("No books found.", exception.Message);
+
         }
-
-
     }
 }
--- LibroConsoleAPI.IntegrationTests/_GetSpecificIntegrationTests.cs
+++ LibroConsoleAPI.IntegrationTests/_GetSpecificIntegrationTests.cs
@@ -1,4 +1,6 @@
-﻿using LibroConsoleAPI.Business.Contracts;
+﻿using LibroConsoleAPI.Business;
+using LibroConsoleAPI.Business.Contracts;
+using LibroConsoleAPI.DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,7 @@ namespace LibroConsoleAPI.IntegrationTests.XUnit
     {
         private readonly BookManagerFixture _fixture;
         private readonly TestLibroDbContext _dbContext;
-        private readonly IBookManager _bookManager;
+        private readonly BookManager _bookManager;
 
         public _GetSpecificIntegrationTests()
         {
@@ -19,19 +21,45 @@ namespace LibroConsoleAPI.IntegrationTests.XUnit
             _dbContext = _fixture.DbContext;
             _bookManager = _fixture.BookManager;
         }
+        //Arrange
 
+        //Act
+
+        //Assert
 
         [Fact]
         public async Task GetSpecificAsync_WithValidIsbn_ShouldReturnBook()
         {
-            throw new NotImplementedException();
+            //Arrange
+            await DatabaseSeeder.SeedDatabaseAsync(_dbContext, _bookManager);
+
+            //Act
+            string specificBookISBN = "9780330258644";
+            string specificBookTitle = "The Catcher in the Rye";
+
+            var specificBook = await _bookManager.GetSpecificAsync(specificBookISBN);
+
+            //Assert
+            Assert.Equal(10, _dbContext.Books.Count());
+
+            Assert.Equal(specificBook.Title, specificBookTitle);
+            Assert.Equal(specificBook.ISBN, specificBookISBN);
         }
 
 
         [Fact]
         public async Task GetSpecificAsync_WithInvalidIsbn_ShouldThrowKeyNotFoundException()
         {
-            throw new NotImplementedException();
+            //Arrange
+            await DatabaseSeeder.SeedDatabaseAsync(_dbContext, _bookManager);
+
+            //Act
+            string invalidSBN = "MIX9780330258644MIX";
+
+
+            //Assert
+            var exception = await Assert.ThrowsAsync<KeyNotFoundException>(()=> _bookManager.GetSpecificAsync(invalidSBN));
+            Assert.Equal($"No book found with ISBN: {invalidSBN}", exception.Message);
         }
 
 
